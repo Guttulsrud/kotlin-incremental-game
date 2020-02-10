e7d9a3b7e6ef74456a75796bd21f7762a1d7d1b0
Håkon Guttulsrud <guttulsrud@outlook.com>
2020-02-10 10:12:08 +0100

    Initial setup

--- app/src/main/java/com/example/incremental/MainActivity.kt
+++ app/src/main/java/com/example/incremental/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.incremental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        textView.text = "0"
+        button.setOnClickListener {
+            counter++
+            textView.text = counter.toString()
+        }
     }
 }
